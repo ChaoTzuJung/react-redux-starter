d435cb2eda39c39f369cb7c08188e274d68fc738
趙子榮 <zhaozirong@zhaozirongdeiMac.local>
2021-09-22 10:28:58 +0800

    fix: Add redux

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import { useSelector, useDispatch } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { actionCreators } from './state';
 import './App.css';
 
 function App() {
+  const state = useSelector(state => state.account);
+  const dispatch = useDispatch();
+
+  const { despositMoney, withdrawMoney } = bindActionCreators(actionCreators, dispatch)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state}
+      <button onClick={() => despositMoney(1000)}>Desposit</button>
+      <button onClick={() => withdrawMoney(1000)}>Withdraw</button>
     </div>
   );
 }
